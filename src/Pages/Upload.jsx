13605e17b821e5371295831bbb2ea11ebce8270d
wuraola babalola <babalolawuraola321@gmail.com>
2024-10-18 19:39:41 +0100

    file size fix

--- src/Pages/Upload.jsx
+++ src/Pages/Upload.jsx
@@ -28,7 +28,7 @@ const UploadPage = () => {
     });
 
     try {
-      const response = await axios.post('http://localhost:5000/home', formData, {
+      const response = await axios.post('https://ponmile-ati-dayo.onrender.com/home', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
